e488f9c70657e888b496f5e24318c3010f1df0d6
Adisu <addisuderrese@hotmail.com>
2026-06-05 21:37:46 +0300

    correct file rank typo

--- engine.py
+++ engine.py
@@ -89,7 +89,7 @@ class Move():
         self.endRow = sqEnd[0]
         self.endCol = sqEnd[1]
 
-        rankToFile = {0: "e", 1:"b", 2:"c", 3:"d", 4:"e", 5:"f", 6:"g", 7: "h"}
+        rankToFile = {0: "a", 1:"b", 2:"c", 3:"d", 4:"e", 5:"f", 6:"g", 7: "h"}
         rowToRank = {0 : 8, 1:7, 2:6, 3:5, 4:4, 5:3, 6:2, 7:1}
 
         notation = ""
