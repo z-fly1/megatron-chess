0d476ba6a0e6ad580ca9069719626122a894d595
Adisu <addisuderrese@hotmail.com>
2026-06-06 08:48:23 +0300

    add captures to black pawns

--- engine.py
+++ engine.py
@@ -67,6 +67,14 @@ class GameState():
                 if r == 1 and self.board[r+2][c] == "--":
                     moves.append(Move((r,c), (r+2, c), self.board))
 
+            if c-1 >=0:
+                if self.board[r+1][c-1][0] == "w":
+                    moves.append(Move((r,c), (r+1, c-1), self.board))
+
+            if c+1 <= 7:
+                if self.board[r+1][c+1][0] == "w":
+                    moves.append(Move((r,c), (r+1, c+1), self.board))
+
 
 
 
